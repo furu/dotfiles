19db7835517aafac73b370d5b7507845932ad1df
furu <ba09219@gmail.com>
2025-05-20 14:06:33 +0900

    Update

---
+++ macos-defaults.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o verbose
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+# https://macos-defaults.com/
+
+# Dock
+defaults write com.apple.dock "persistent-apps" -array
+defaults write com.apple.dock "autohide" -bool "true"
+killall Dock
